b3255fe305357fcbf24ab65248207f6cc8610974
Joshua-Lee102 <joshuaplee1@gmail.com>
2024-04-19 17:50:11 -0400

    update import statements

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from '/src/header'
-import Body from '/src/body'
+import Header from '/src/header.jsx'
+import Body from '/src/body.jsx'
 
 function App() {
   return (
